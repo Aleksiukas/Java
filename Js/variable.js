c6863a8d245470c1ec9fa5975b3378f8b0354429
Aleksiukas <edgaras.aleksiukas@gmail.com>
2020-04-27 14:13:11 +0300

    variables js

---
+++ Js/variable.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*kintamuju tipai:
+- const  - nekeiciamas niekada
+- let    - keiciamas
+- var    - keiciamas, naujasnis uz let, beveik nenaudojamas.
+*/
+
+ const manoVardas = 'Edga'; //tekstas/
+ const akiuSkaicius = 2;    //skaicius/
+ const turiGalva =true;     //logine reiksme  true/false
+
+ console.log (manoVardas)
+ console.log (akiuSkaicius)
+ console.log (turiGalva)
